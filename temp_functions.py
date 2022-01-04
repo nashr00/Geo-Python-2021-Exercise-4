1090f7960f6059b79fc48c2ff01acc8d48aca2c4
[nashr00] <[nashrrullah@gmail.com]>
2022-01-05 00:03:31 +0700

    temp_functions added

---
+++ temp_functions.py
@@ -0,0 +1,17 @@
+'''
+A function to convert temperatures in Fahrenheit to Celsius and classifies the values into four classes
+'''
+
+def fahr_to_celsius(temp_fahrenheit):
+    converted_temp = (temp_fahrenheit - 32) / 1.8
+    return converted_temp
+
+def temp_classifier(temp_celsius):
+    if temp_celsius < -2:
+        return 0
+    elif temp_celsius < 2:
+        return 1
+    elif temp_celsius < 15:
+        return 2
+    elif temp_celsius >= 15:
+        return 3   
